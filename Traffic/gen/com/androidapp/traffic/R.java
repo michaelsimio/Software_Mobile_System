e7b377488cfbadf0a9aa89c0c4491c5431c9d66a
Geoffrey Oh <geoffreyoh@gmail.com>
2013-04-30 14:31:48 -0400

    Organization
    Cleaned up some older unnecessary files (old states can be recovered by
    rolling back commits) so that only the current version is in the repo.
    This is the version with the feature of obtaining user's address.

--- Traffic/gen/com/androidapp/traffic/R.java
+++ Traffic/gen/com/androidapp/traffic/R.java
@@ -23,17 +23,22 @@ public final class R {
         public static final int TextView01=0x7f080004;
         public static final int button1=0x7f080001;
         public static final int editText1=0x7f080003;
-        public static final int imageView1=0x7f080006;
-        public static final int radio0=0x7f080009;
-        public static final int radio1=0x7f08000a;
-        public static final int radio2=0x7f08000b;
-        public static final int radio3=0x7f08000c;
-        public static final int radio4=0x7f08000d;
-        public static final int radioGroup1=0x7f080007;
-        public static final int radioGroup2=0x7f080008;
-        public static final int spinner2=0x7f080005;
-        public static final int stv=0x7f08000e;
+        public static final int imageView1=0x7f080008;
+        public static final int radio0=0x7f08000b;
+        public static final int radio1=0x7f08000c;
+        public static final int radio2=0x7f08000d;
+        public static final int radio3=0x7f08000e;
+        public static final int radio4=0x7f08000f;
+        public static final int radioGroup1=0x7f080009;
+        public static final int radioGroup2=0x7f08000a;
+        public static final int spinner1=0x7f080005;
+        public static final int spinner2=0x7f080007;
+        public static final int stv=0x7f080013;
         public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080006;
+        public static final int textView3=0x7f080010;
+        public static final int textView4=0x7f080011;
+        public static final int textView5=0x7f080012;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
